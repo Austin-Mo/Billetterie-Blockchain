ec967a554d6d0cf8885067fe90435cc9149d6d3a
THAVEL Florian <florian.thavel@groupe-esigelec.org>
2021-12-15 11:21:17 +0100

    modif page de connexion et navbar

--- Front/connexionPage.php
+++ Front/connexionPage.php
@@ -3,9 +3,9 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Project Manager</title>
+  <title>Billeterie Match</title>
   <!-- Favicons -->
-  <link href="assets/img/logo-esig.png" rel="icon">
+  <link href="assets/img/logoDragon.jpg" rel="icon">
   <!-- Bootstrap CSS -->
   <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.2/css/bootstrap.min.css'>
   <!-- Font Awesome CSS -->
@@ -22,7 +22,7 @@
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
 
 </head>
-<body  style="background-color: #563cec;">
+<body  style="background-color: rgb(252 190 25);">
 
 
  <main>
@@ -33,67 +33,87 @@
           <div class="shadow-lg card" style="border-radius: 1rem;">
             <div class="row g-0">
               <div class="col-md-6 col-lg-5 d-none d-md-block">
-                <img src="assets/img/logoDragon.jpg" style="height:25vw; margin: 7vw 0 0 1vw;" />
-              </div>
-              <div class="col-md-6 col-lg-7 d-flex align-items-center">
-                <div class="card-body p-4 p-lg-5 text-black">
-
-
-                  
-
-
-                  <form action="tt_connexion.php" method ="POST">
-
-                    <div class="d-flex align-items-center mb-3 pb-1">
-                      <span class="h1 fw-bold mb-0">Project Manager</span>
-                    </div>
-
-                    <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Connectez-vous à votre compte :</h5>
-
-                    <?php 
-                    if(isset($_COOKIE['emailSigninError']))
-                    {
-                      if ($_COOKIE['emailSigninError']==1)
-                      {
-                        echo "<h6 style='color:red; font-style: italic;'>Pas de compte associé à cet email</h6>";
-                      }
-                    }
-                    if(isset($_COOKIE['passwordSigninError']))
-                    {
-                      if ($_COOKIE['passwordSigninError']==1)
-                      {
-                        echo "<h6 style='color:red; font-style: italic;'>Mauvais mot de passe</h6>";
-                      }
-                    }
-                    ?>
-
-                    <div class="form-outline mb-4">
-                      <input type="email" id="email" class="form-control form-control-lg" name="email" required/>
-                      <label class="form-label" for="email">Email</label>
-                    </div>
-
-                    <div class="form-outline mb-4">
-                      <input type="password" id="password" class="form-control form-control-lg" name="password" required/>
-                      <label class="form-label" for="password">Mot de passe</label>
-                    </div>
-
-                    <div class="pt-1 mb-4">
-                      <button class="btn btn-dark btn-lg btn-block" type="submit">Se connecter</button>
-
-                    </div>
-
-                    <a class="small text-muted" href="#!">Mot de passe oublié?</a>
-                    <p class="mb-5 pb-lg-2" style="color: #393f81;">Vous n'avez pas de compte? <a href="creationCompte.php" style="color: #393f81;">créez-en un !</a></p>
-                  </form>
-
+                <div class="container-img">
+                  <img src="assets/img/logoDragon.png" style="height:25vw; margin: 7vw 0 0 1vw;" />
                 </div>
+                <style type="text/css">
+                  .container-img{
+                   display:block;
+                   overflow:hidden;
+                 }
+                 .container-img img{
+                   width:100%;
+                   height:100%;
+                   -webkit-transition: all 0.2s;
+                   -moz-transition: all 0.2s;
+                   -ms-transition: all 0.2s;
+                   -o-transition: all 0.2s;
+                   transition: all 0.2s;
+                 }
+                 .container-img:hover img{
+                   transform:rotate(5deg) scale(1.2);
+                 }
+               </style>
+             </div>
+             <div class="col-md-6 col-lg-7 d-flex align-items-center">
+              <div class="card-body p-4 p-lg-5 text-black">
+
+
+
+
+
+                <form action="tt_connexion.php" method ="POST">
+
+                  <div class="d-flex align-items-center mb-3 pb-1">
+                    <span class="h1 fw-bold mb-0">Billeterie Match</span>
+                  </div>
+
+                  <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Connectez-vous à votre compte :</h5>
+
+                  <?php 
+                  if(isset($_COOKIE['emailSigninError']))
+                  {
+                    if ($_COOKIE['emailSigninError']==1)
+                    {
+                      echo "<h6 style='color:red; font-style: italic;'>Pas de compte associé à cet email</h6>";
+                    }
+                  }
+                  if(isset($_COOKIE['passwordSigninError']))
+                  {
+                    if ($_COOKIE['passwordSigninError']==1)
+                    {
+                      echo "<h6 style='color:red; font-style: italic;'>Mauvais mot de passe</h6>";
+                    }
+                  }
+                  ?>
+
+                  <div class="form-outline mb-4">
+                    <input type="email" id="email" class="form-control form-control-lg" name="email" required/>
+                    <label class="form-label" for="email">Email</label>
+                  </div>
+
+                  <div class="form-outline mb-4">
+                    <input type="password" id="password" class="form-control form-control-lg" name="password" required/>
+                    <label class="form-label" for="password">Mot de passe</label>
+                  </div>
+
+                  <div class="pt-1 mb-4">
+                    <button class="btn btn-dark btn-lg btn-block" type="submit">Se connecter</button>
+
+                  </div>
+
+                  <a class="small text-muted" href="#!">Mot de passe oublié?</a>
+                  <p class="mb-5 pb-lg-2" style="color: #393f81;">Vous n'avez pas de compte? <a href="creationCompte.php" style="color: #393f81;">créez-en un !</a></p>
+                </form>
+
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </section>
+  </div>
+</section>
 
 </main>
 </body>
--- Front/navbar.php
+++ Front/navbar.php
@@ -1,15 +1,15 @@
   <!-- ======= Header ======= -->
   <header id="header" class="fixed-top ">
     <div class="container d-flex align-items-center justify-content-between">
-      <h1 class="logo"><a href="index.php">Project Manager</a></h1>
+      <h1 class="logo"><a href="index.php"></a>Billeterie Match</h1>
 
 
 <!-- NAVBAR -->
       <nav id="navbar" class="navbar">
         <ul>
           <li><a class="nav-link scrollto active" href="#hero">Accueil</a></li>
-          <li><a class="nav-link scrollto" href="#about">À propos</a></li>
-          <li><a class="nav-link scrollto" href="#projects">Projets</a></li>
+          <li><a class="nav-link scrollto" href="#compte">Mon compte</a></li>
+          <li><a class="nav-link scrollto" href="#projects">Panier</a></li>
           <li><a class="nav-link scrollto" href="#contact">Contacts</a></li>
           <li><a class="getstarted scrollto" href="connexionPage.php">Se connecter</a></li>
         </ul>
