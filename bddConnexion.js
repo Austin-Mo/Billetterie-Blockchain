794e2d0d31df94374f2edf5cb3dc61ff3b1b48a1
lucas lebrun <lucas.lebrun@groupe-esigelec.org>
2022-01-07 11:42:38 +0100

    conexion

--- bddConnexion.js
+++ bddConnexion.js
@@ -1,33 +1,19 @@
-const { Sequelize } = require('sequelize');
-
-var sequelize = new Sequelize('dragon_base', 'lucas', '6532', {
-  host : 'localhost',
-  dialect: 'mariadb'
-})
-
-try {
-  await sequelize.authenticate();
-  console.log('Connection has been established successfully.');
-} catch (error) {
-  console.error('Unable to connect to the database:', error);
-}
-
-
 const mariadb = require('mariadb');
 const pool = mariadb.createPool({
-     host: 'mydb.com', 
-     user:'myUser', 
-     password: 'myPassword',
-     connectionLimit: 5
+     host: 'localhost', 
+     user:'lucas', 
+     password: '6532',
+     connectionLimit: 5,
+     database: "dragon_base"
 });
 async function asyncFunction() {
   let conn;
   try {
   conn = await pool.getConnection();
-  const rows = await conn.query("SELECT 1 as val");
-  console.log(rows); //[ {val: 1}, meta: ... ]
-  const res = await conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
-  console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+  const rows = await conn.query("SELECT * FROM utilisateur");
+  console.log(rows);
+  const res = await conn.query("INSERT INTO utilisateur value (?, ?, ?, ?, ?, ?, ?)", [0, "mariadb", "mariadb", "mariadb", "mariadb@gmail.com", "mariadb", 0]);
+  console.log(res);
 
   } catch (err) {
   throw err;
--- exceptions.js
+++ exceptions.js
@@ -1,19 +1,18 @@
 const mariadb = require('mariadb');
 const pool = mariadb.createPool({
-     host: 'localhost:80', 
+     host: 'localhost', 
      user:'lucas', 
      password: '6532',
-     connectionLimit: 5
+     connectionLimit: 5,
+     database: "dragon_base"
 });
 pool.getConnection()
     .then(conn => {
     
-      conn.query("SELECT 1 as val")
+      conn.query("SELECT * FROM utilisateur")
         .then((rows) => {
-          console.log(rows); //[ {val: 1}, meta: ... ]
-          //Table must have been created before 
-          // " CREATE TABLE myTable (id int, val varchar(255)) "
-          return conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
+          console.log(rows);
+          return conn.query("INSERT INTO utilisateur value (?, ?, ?, ?, ?, ?, ?)", [0, "mariadb", "mariadb", "mariadb", "mariadb@gmail.com", "mariadb", 0]);
         })
         .then((res) => {
           console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
