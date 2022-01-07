71a57e30e238461e91ea06f9508e7c2d18b82c56
lucas lebrun <lucas.lebrun@groupe-esigelec.org>
2022-01-07 09:19:30 +0100

    1 commit

--- bddConnexion.js
+++ bddConnexion.js
@@ -11,3 +11,27 @@ try {
 } catch (error) {
   console.error('Unable to connect to the database:', error);
 }
+
+
+const mariadb = require('mariadb');
+const pool = mariadb.createPool({
+     host: 'mydb.com', 
+     user:'myUser', 
+     password: 'myPassword',
+     connectionLimit: 5
+});
+async function asyncFunction() {
+  let conn;
+  try {
+  conn = await pool.getConnection();
+  const rows = await conn.query("SELECT 1 as val");
+  console.log(rows); //[ {val: 1}, meta: ... ]
+  const res = await conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
+  console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+
+  } catch (err) {
+  throw err;
+  } finally {
+  if (conn) return conn.end();
+  }
+}
