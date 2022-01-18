99a87942709acf4944738d930a19105bec2aa768
lucas lebrun <lucas.lebrun@groupe-esigelec.org>
2022-01-18 14:41:00 +0100

    commit

---
+++ billeterie_smart_contract.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.10;
+
+contract Ticket {
+    
+    string homeTeam;
+    string guestTeam;
+    string matchDate;
+    uint256 ticketPrice;
+    uint256 numberOfTickets;
+    address owner;
+    mapping (address => uint256) public ticketHolders;
+
+    constructor(string memory _homeTeam, string memory _guestTeam, string memory _matchDate, uint256 _numberOfTickets, uint256 _ticketPrice) {
+        homeTeam = _homeTeam; 
+        guestTeam = _guestTeam; 
+        matchDate = _matchDate;
+        numberOfTickets = _numberOfTickets;
+        ticketPrice = _ticketPrice;
+        owner = msg.sender;
+    }
+
+    function buyTicket(address _user, uint256 _amount) public payable {
+        require(numberOfTickets >= _amount);
+        require(msg.value == ticketPrice * _amount);
+        addTickets(_user, _amount);
+        numberOfTickets = numberOfTickets - _amount;
+    }
+
+    function useTickets(address _user, uint256 _amount) public {
+        subTickets(_user, _amount);
+    }
+
+    function addTickets(address _user, uint256 _amount) internal {
+        ticketHolders[_user] = ticketHolders[_user] + _amount;
+    }
+
+    function subTickets(address _user, uint256 _amount) internal {
+        require(ticketHolders[_user] >= _amount, "You do not have anough tickets.");
+        ticketHolders[_user] = ticketHolders[_user] - _amount;
+    }
+
+    function withdraw() public {
+        require(msg.sender == owner, "You are not the owner.");
+        (bool success, ) = payable(owner).call{value: address(this).balance}("");
+        require(success);
+    }
+
+    function getMatch() public view returns(string memory, string memory, string memory, uint256, uint256){
+        return (homeTeam, guestTeam, matchDate, numberOfTickets, ticketPrice);
+   }
+
+}
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////
+
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.10;
+
+contract Billetterie {
+
+    event NewAddress(uint taille, address address_billet);
+
+    address[] private addresses;
+
+    function addAddress(address address_billet) public {
+        addresses.push(address_billet);
+        emit NewAddress(addresses.length, address_billet);
+    }
+
+    // function that returns entire adresses
+    function getAddresses() public view returns (address[] memory) {
+        return addresses;
+    }
+}
